1767f86adf7c126f34c8ed53a17e1bd25a02465d
montanmimi06-ui <montanmimi06@gmail.com>
2026-03-23 21:08:03 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const admin = require("firebase-admin");
+
+const app = express();
+app.use(express.json());
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+app.post("/send", async (req, res) => {
+  const { token, data } = req.body;
+
+  try {
+    await admin.messaging().send({
+      token: token,
+      notification: {
+        title: "Nova notificação 💌",
+        body: "Você recebeu algo",
+      },
+      data: data || {},
+    });
+
+    res.send("Enviado!");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Erro ao enviar");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
